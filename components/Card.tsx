ad17ad2a8ab2f81056fbb4de9bf251fa91ca705b
Aneta <anc@praqma.net>
2022-08-13 14:10:22 +0200

    added texts for homepage

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
-import { slugify } from "../utils/slugify";
-import Link from "next/link";
-import CardType from "../types/card.type";
-import { ReactNode } from "react";
+import Image from 'next/image';
+import { slugify } from '../utils/slugify';
+import Link from 'next/link';
+import CardType from '../types/card.type';
+import { ReactNode } from 'react';
 
 interface CardProps extends CardType {
   background?: string;
@@ -28,27 +28,32 @@ export default function Card({
         <div style={{ padding: 12 }}>
           <Image
             src={`/cards/${post?.image}`}
-            alt="blue"
+            alt='blue'
             className={`half filter-yellow`}
             height={80}
             width={80}
-            objectFit="contain"
+            objectFit='contain'
           />
         </div>
       )}
-      {post?.type && <h4 className="type bg-purplelight">{post?.type}</h4>}
+      {post?.type && <h4 className='type bg-purple'>{post?.type}</h4>}
       {post?.title && (
         <Link href={`/events/${slugify(post?.title)}`}>
-          <h4 style={{ marginBottom: "-8px", cursor: "pointer" }}>
+          <h4
+            style={{
+              marginBottom: '-8px',
+              cursor: 'pointer',
+            }}
+          >
             {post?.title}
           </h4>
         </Link>
       )}
-      {post?.address && <h5 className="bolded salmon">{post?.address}</h5>}
+      {post?.address && <h5 className='bolded purple'>{post?.address}</h5>}
       {post?.address && <h5>{post?.text}</h5>}
       {post?.opening && (
-        <h5 className="">
-          <span className="salmon bolded">open </span>
+        <h5 className=''>
+          <span className='purple bolded'>open </span>
           {post?.opening}
         </h5>
       )}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,28 @@
-import fs from "fs";
-import matter from "gray-matter";
-import path from "path";
-import CardsList from "../components/CardsList/CardsList";
-import Paragraph from "../components/Paragraph/Paragraph";
-import StarsDivider from "../components/StarsDivider";
-import TagsList from "../components/TagsList";
-import { DefaultLayout } from "../layouts/DefaultLayout/DefaultLayout";
-import Intro from "../components/Intro/Intro";
-import ImageSection from "../components/ImageSection/ImageSection";
-import CategoryList from "../components/CategoryList";
-import Link from "next/link";
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+import CardsList from '../components/CardsList/CardsList';
+import Paragraph from '../components/Paragraph/Paragraph';
+import StarsDivider from '../components/StarsDivider';
+import TagsList from '../components/TagsList';
+import { DefaultLayout } from '../layouts/DefaultLayout/DefaultLayout';
+import Intro from '../components/Intro/Intro';
+import ImageSection from '../components/ImageSection/ImageSection';
+import CategoryList from '../components/CategoryList';
+import Link from 'next/link';
+import sections from '../texts/home.json';
+import styles from './Home/Home.module.scss';
 
 export async function getStaticProps() {
   // get files from the directory
-  const files = fs.readdirSync(path.join("pages/posts"));
+  const files = fs.readdirSync(path.join('pages/posts'));
   const posts = files.map((filename) => {
     // get slug
-    const slug = filename.replace(".md", "");
+    const slug = filename.replace('.md', '');
     // get all frontmatter here:
     const markdownWithMeta = fs.readFileSync(
-      path.join("pages/posts", filename),
-      "utf-8"
+      path.join('pages/posts', filename),
+      'utf-8'
     );
     const { data: frontmatter } = matter(markdownWithMeta);
 
@@ -38,40 +40,31 @@ interface HomeProps {
 }
 
 export default function Home({ posts }: HomeProps) {
-  const title = "Home";
+  const title = 'Home';
   return (
     <DefaultLayout title={title}>
-      <Intro background="/14.jpeg" />
+      <Intro background='/14.jpeg' />
 
-      <section className="center">
-        <h2>Who makes South Harbour</h2>
-
-        <Paragraph>
-          To see the exact date and all sorts of upcoming activities, check the
-          facebook site of or click any of the place links
-        </Paragraph>
+      <section className='center'>
+        <h2>{sections.members.title}</h2>
+        <Paragraph>{sections.members.text}</Paragraph>
         <br />
         {/* <CategoryList posts={posts} /> */}
         {/* <TagsList posts={posts} /> */}
-        <CardsList posts={posts} regular type="main" background="gray" main />
-        <Link href="/members">
-          <button>members</button>
+        <CardsList posts={posts} regular type='main' background='gray' main />
+        <Link href='/members'>
+          <button>{sections.members.button}</button>
         </Link>
       </section>
 
-      <section className="bg-black center">
-        <h3 className="purple">coming up next</h3>
-        <h2
-          style={{ fontSize: 54, lineHeight: 1, maxWidth: 900, margin: "auto" }}
-        >
-          Sydhavnens <br />
-          festival 2022
+      <section className='bg-black center'>
+        <h3 className='purple'>{sections.next.subtitle}</h3>
+        <h2 className={styles.largeTitle}>{sections.next.title}</h2>
+        <h2 style={{ fontSize: 54 }} className='pink'>
+          {sections.next.date}
         </h2>
-        <h2 style={{ fontSize: 54 }} className="pink">
-          20. - 22.8.
-        </h2>
-        <a href="https://www.facebook.com/events/1379108515836403">
-          <button>facebook event</button>
+        <a href='https://www.facebook.com/events/1379108515836403'>
+          <button>{sections.next.button}</button>
         </a>
 
         {/* <Paragraph>
@@ -100,70 +93,47 @@ export default function Home({ posts }: HomeProps) {
         </div> */}
       </section>
 
-      <section className="bg-pink">
-        <div style={{ maxWidth: 1200, margin: "auto" }}>
-          <h2>Vision & values</h2>
-          <p style={{ fontSize: 20, maxWidth: 800 }}>
-            Each one teach one is a series of small workshops organised by
-            anybody who has anything to bring to the table. The idea behind is
-            that each of us has a skill or know-how that many of us could
-            benefit from learning. Hold in Frontloberne. Check website for the
-            upcoming events. Link.
+      <section className='bg-pink'>
+        <div style={{ maxWidth: 1200, margin: 'auto' }}>
+          <h2>{sections.values.title}</h2>
+          <p className='large' style={{ maxWidth: 800 }}>
+            {sections.values.text}
           </p>
           <p>see all the memmbers</p>
-          <button>Read more</button>
+          <button>{sections.values.button}</button>
         </div>
       </section>
-      <ImageSection background="/20.jpeg" />
-      <section className="bg-red center">
-        <h2>Open for Public</h2>
-        <Paragraph>
-          Many initiatives in Sydhaven offer different creative workshops open
-          for public some of them free of charge, courses, cultural events,
-          provide a space for connecting or developing ones skills or simply
-          just having a chat. To see the exact date and all sorts of upcoming
-          activities, check the facebook site of{" "}
-          <a href="https://www.facebook.com/groups/154685458042586/events">
-            Sydhaven
-          </a>{" "}
-          or browse the list of members
-        </Paragraph>
+
+      <ImageSection background='/20.jpeg' />
+
+      <section className='bg-red center'>
+        <h2>{sections.open.title}</h2>
+        <Paragraph>{sections.open.text}</Paragraph>
         <StarsDivider />
-        <CardsList posts={posts} opening="present" background="gray" />
+        <CardsList posts={posts} opening='present' background='gray' />
       </section>
 
-      <section className="bg-pink">
-        <div className="flex-center" style={{ maxWidth: 1400, margin: "auto" }}>
-          <div style={{ textAlign: "right", maxWidth: "50%" }}>
-            <h2>Community & Sustainability</h2>
-            <p>
-              Due to placement at the border of very noisy industrial
-              neighbourhood and city center, Sydhaven is also an important place
-              where many initiatives that may not be able to survive elsewhere
-              are located. recycle and redistribute, trying to make a
-              difference, help vulnerable communities and collect and
-              redistribute resources in more sustainable way
-            </p>
+      <section className='bg-pink'>
+        <div className='flex-center' style={{ maxWidth: 1400, margin: 'auto' }}>
+          <div style={{ textAlign: 'right', maxWidth: '50%' }}>
+            <h2>{sections.second.title}</h2>
+            <p>{sections.second.text}</p>
             <StarsDivider />
           </div>
-          <div style={{ maxWidth: "50%" }}>
-            <CardsList posts={posts} tag="community" />
+          <div style={{ maxWidth: '50%' }}>
+            <CardsList posts={posts} tag='community' />
           </div>
         </div>
       </section>
-      <ImageSection background="/9.jpeg" />
-      <section className="bg-red center" style={{ paddingBottom: 120 }}>
-        <h4 className="purple">creative business</h4>
-        <h2>Culture Makers & Value Creators</h2>
-        <Paragraph>
-          Sydhaven is home to many social and creative initiatives or platform
-          that are trying to help young & creative people to become connected,
-          develop their skills and inspire each other. Often centered around
-          meaningful activities and purposes.
-        </Paragraph>
-        <StarsDivider />
 
-        <CardsList posts={posts} tag="event makers" background="purple" />
+      <ImageSection background='/9.jpeg' />
+
+      <section className='bg-red center' style={{ paddingBottom: 120 }}>
+        <h4 className='purple'>{sections.third.subtitle}</h4>
+        <h2>{sections.third.title}</h2>
+        <Paragraph>{sections.third.text}</Paragraph>
+        <StarsDivider />
+        <CardsList posts={posts} tag='event makers' background='purple' />
       </section>
     </DefaultLayout>
   );
